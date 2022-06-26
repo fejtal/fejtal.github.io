0fcc87e5b3bcd0613c747b0c6e6379a1b25ac7c4
fejtal <kamil@czekaj.us>
2022-06-27 00:29:52 +0200

    asds

--- src/components/Breadcrumb/Breadcrumb.svelte
+++
@@ -1,6 +0,0 @@
-<div class="text-sm breadcrumbs px-2">
-    <ul>
-      <li><a href='https://google.com'>Home</a></li> 
-      <li><a href='https://google.com'>Newsletter</a></li> 
-    </ul>
-</div>
