5c5af3df5c5ed37ec784221b5f20ead28f45632c
fejtal <kamil@czekaj.us>
2022-06-27 00:29:52 +0200

    asds

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const entry = 'pages/index.svelte-8d4526eb.js';
-export const js = ["pages/index.svelte-8d4526eb.js","chunks/index-7cfb3f74.js","chunks/index-d2d4d66a.js"];
-export const css = ["assets/pages/index.svelte-7cf48b6b.css"];
--- src/components/Form/store.js
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-
-export let pageTypeHome = "home"; // will make a functionality so it's dynamic
-
-export let user = writable({
-  email: "",
-  pageType: pageTypeHome
-})
